a36ef7fc4ba245fd3433859d67d10028daa65682
Schelde Jonas <scheldejonas@gmail.com>
2017-02-19 16:58:30 +0100

    CA-CA1, Created First Test Object

--- CA/CA1/chat-server-two-gruppe-ti/src/test/java/service/ChatProtocolTest.java
+++ CA/CA1/chat-server-two-gruppe-ti/src/test/java/service/ChatProtocolTest.java
@@ -1,7 +1,27 @@
 package service;
 
+import controller.ChatServer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.net.Socket;
+
 /**
  * Created by scheldejonas on 18/02/2017.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ChatProtocolTest {
+
+    @Mock
+    private ChatServer chatServer;
+
+    @Mock
+    private Socket socket;
+
+    @InjectMocks
+    private ClientThread clientThread = new ClientThread(socket);
+
 }
